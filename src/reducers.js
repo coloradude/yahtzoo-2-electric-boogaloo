a5c881bd7e7ae7b6bf99abdd84ddcad8d9397f03
coloradude <steezeven@gmail.com>
2017-12-12 16:10:54 -0700

    added some scoreing functions

--- src/reducers.js
+++ src/reducers.js
@@ -3,25 +3,43 @@ const genericNumsScore = (dice, num) => {
 }
 
 const threeOrFourOfAKindScore = (dice, num) => {
-  const reduced = dice.reduce((curr, next) => curr[nex.value] ? curr[next.value] +  : curr[nex.value] = 1, {})
+  const reduced = dice.reduce((curr, next) => {
+    curr[next.value] = curr[next.value] + 1 || 1
+    return curr
+  }, {})
 
-  const finalScore = 0
+  let finalScore = 0
 
-  for (item in reduced) {
-    if (Number(reduced[item]) === num) {
-      finalScore = Object.getOwnPropertyName(reduced[item]) * num
+  for (let item in reduced) {
+    
+    if (Number(reduced[item]) >= num) {
+      finalScore = dice.reduce((curr, next) => curr + next.value, 0)
     }
   }
-
   return finalScore
 }
 
-const 
+const fullHouseScore = dice => {
+  const scores = dice.reduce((curr, next) => {
+    curr[next.value] = curr[next.value] + 1 || 1
+    return curr
+  }, {})
+
+  const propertyNames = Object.getOwnPropertyNames(scores)
+  return propertyNames.length == 2 && (scores[propertyNames[0]] == 3 || scores[propertyNames[1]] == 3) ? 25 : 0
+}
+
+const yahtzooScore = dice => {
+  const scores = dice.reduce((curr, next) => {
+    curr[next.value] = curr[next.value] + 1 || 1
+    return curr
+  }, {})
+   return Object.getOwnPropertyNames(scores).length === 1 ? 50 : 0
+}
+
+
 
 const calculateScores = (state, action) => {
-
-  console.log(action)
-  console.log(state, 'Reducer state')
 
   switch(action.type){
     case 'CALCULATE_VALUES': 
@@ -35,45 +53,73 @@ const calculateScores = (state, action) => {
           die
       })
 
-    const ones = genericNumsScore(dice, 1))
-    const twos = genericNumsScore(dice, 2))
-    const threes = genericNumsScore(dice, 3))
-    const fours = genericNumsScore(dice, 4))
-    const fives = genericNumsScore(dice, 5))
-    const sixes = genericNumsScore(dice, 6))
+    const ones = genericNumsScore(dice, 1)
+    const twos = genericNumsScore(dice, 2)
+    const threes = genericNumsScore(dice, 3)
+    const fours = genericNumsScore(dice, 4)
+    const fives = genericNumsScore(dice, 5)
+    const sixes = genericNumsScore(dice, 6)
     const threeOfAKind = threeOrFourOfAKindScore(dice, 3)
     const fourOfAKind = threeOrFourOfAKindScore(dice, 4)
+    const fullHouse = fullHouseScore(dice)
+    const yahtzoo = yahtzooScore(dice)
+    const chance = dice.reduce((curr, next) => curr + next.value, 0)
 
 
-      newState.gameBoard = {
-        ones: {
-          score: ones,
-          isActive: true
-        },
-        twos: {
-          score: twos,
-          isActive: true
-        },
-        threes: {
-          score: threes,
-          isActive: true
-        },
-        fours: {
-          score: fours,
-          isActive: true
-        },
-        fives: {
-          score: fives,
-          isActive: true
-        },
-        sixes: {
-          score: sixes,
-          isActive: true
-        },
-        ones: {
-          score: ones,
-          isActive: true
-        },
+    newState.gameBoard = {
+      ones: {
+        score: ones,
+        isActive: true
+      },
+      twos: {
+        score: twos,
+        isActive: true
+      },
+      threes: {
+        score: threes,
+        isActive: true
+      },
+      fours: {
+        score: fours,
+        isActive: true
+      },
+      fives: {
+        score: fives,
+        isActive: true
+      },
+      sixes: {
+        score: sixes,
+        isActive: true
+      },
+      threeOfAKind: {
+        score: threeOfAKind,
+        isActive: true
+      },
+      fourOfAKind: {
+        score: fourOfAKind,
+        isActive: true
+      },
+      fullHouse: {
+        score: fullHouse,
+        isActive: true
+      },
+      smallStraight: {
+        score: 1,
+        isActive: true
+      },
+      largeStraight: {
+        score: 1,
+        isActive: true
+      },
+      yahtzee: {
+        score: 1,
+        isActive: yahtzoo
+      },
+      chance: {
+        score: chance,
+        isActive: true
+      }
+    }
         
         
 
@@ -94,6 +140,3 @@ const calculateScores = (state, action) => {
 
 export default calculateScores
 
-const ones = (dice) => {
-    
-}
